d0b015b2e481a7654b58b315ace2aa274be4154e
Deepanshu902 <deepanshubad000@gmail.com>
2025-02-24 12:25:03 +0530

    removed api key

--- Backend/src/utils/cloudinary.js
+++ Backend/src/utils/cloudinary.js
@@ -1,10 +1,13 @@
+import dotenv from "dotenv" // getting order issue as env is loading for cloudinary so also config it here
+dotenv.config()
+
 import { v2 as cloudinary } from 'cloudinary';
 import fs from "fs";
 
-cloudinary.config({ 
-    cloud_name: 'dj6kl9cp3', 
-    api_key: '568328796182512', 
-    api_secret: 'TlocSA0SC4oElZyvqsKWyZv1phw' 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilePath,oldImagePublicId = null) => {
