d61ce59da0c97da10440ab0316c5a02b4a184fec
Deepanshu902 <deepanshubad000@gmail.com>
2025-03-02 15:36:45 +0530

    path,pages and components setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { 
+  Dashboard,
+  LandingPage,
+  Login,
+  Register
+} from "./pages/index"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <></>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ Frontend/src/pages/index.js
@@ -0,0 +1,12 @@
+import Dashboard from "./Dashboard"
+import LandingPage from "./LandingPage"
+import Login from "./Login"
+import Register from "./Register"
+
+
+export {
+    Dashboard,
+    LandingPage,
+    Login,
+    Register
+}
