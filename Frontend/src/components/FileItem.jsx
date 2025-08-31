2f1bae9ebd22d17b0d8be060d7ae01713370dae6
Deepanshu902 <deepanshubad000@gmail.com>
2025-08-31 12:58:54 +0530

    ui ux improvement

--- Frontend/src/components/FileItem.jsx
+++ Frontend/src/components/FileItem.jsx
@@ -55,9 +55,12 @@ const FileItem = ({ file }) => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" /></svg>
             </div>
         )}
+        <a href={file.fileUrl} target="_blank" rel="noopener noreferrer" title="View image in new tab">
         <span className="font-medium text-white">
             {file.filename || "Unnamed File"}
         </span>
+        </a>
+        
       </div>
 
       <div className="flex items-center gap-2">
